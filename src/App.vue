9a3b9d24735cd061955bdab32d0a880f03407aa5
odoe <rrubalcava@esri.com>
2019-09-12 12:45:40 -0700

    vue the wonder

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Header :appTitle="title" />
+    <WebMap />
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header';
+import WebMap from './components/WebMap';
+
+import { title } from './config';
+
+export default {
+    name: 'App',
+    components: { Header, WebMap },
+    data: () => (
+      {
+        title
+      }
+    )
+};
+
+</script>
+
+<style scoped>
+    div  {
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+      margin: 0;
+      width: 100%;
+      height: 100%;
+    }
+</style>
